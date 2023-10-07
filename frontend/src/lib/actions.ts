3cd181879b720d1566b54e4bf6c4f30c5c4f20bc
MaxG <maxpgrabov@gmail.com>
2023-10-07 17:27:53 -0400

    Added Parser to the Front End

--- frontend/src/lib/actions.ts
+++ frontend/src/lib/actions.ts
@@ -34,3 +34,34 @@ export const sendFormData = async (data: FormData) => {
 		console.log(err);
 	}
 };
+
+export const parse = async (data: FormData) => {
+	try{
+		const responseString = data.get('name');
+		// const responseString = "Question: What is the main purpose of the lipid bilayer in the cell membrane?\nA. To provide a barrier between the components of the cell and the outside environment\nB. To act as a threshold for molecules to enter and exit the cell\nC. To define the cell and keep its components separate from outside cells or organisms\nD. To provide a structural component for the cell membrane to be composed of\nAnswer: C. To define the cell and keep its components separate from outside cells or organisms"
+		if(!responseString) return;
+
+		const response = responseString.toString().split("\\n")
+		
+		if(response[5] == ''){
+        response.splice(5, 1)
+		}
+		var question = response[0].slice(10)
+		var correctAnswer = response[5].slice(8,9).charCodeAt(0) - 'A'.charCodeAt(0).valueOf()
+		var answers = []
+		for(var i=1; i<5; i++){
+			answers.push(response[i].slice(3))
+		}
+		let res = {	
+			"question": question,
+			"answer": answers, 
+			"correctAnswer": correctAnswer
+		}
+
+		return res
+	} catch (err) {
+		console.log(err)
+	}
+	
+	
+}
