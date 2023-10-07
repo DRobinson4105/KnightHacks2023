fda1c0b68510dc403e60961fd22b8952804f150e
David Robinson <drobinson4105@gmail.com>
2023-10-07 17:49:34 -0400

    Finished practice question generator function

--- backend/main.py
+++ backend/main.py
@@ -7,38 +7,45 @@ from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 from pdfParser import pdfParse
 from responseParser import parse
+from langchain.cache import InMemoryCache
+import langchain
+langchain.llm_cache = InMemoryCache()
 
 load_dotenv()
 
-courseName = "biology"
+topic = "biology"
 pdfs = ["./notes1.pdf", "./notes2.pdf"]
 
 # Set up text to be stored in vector database
 textSplitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 textSplit = textSplitter.split_text(pdfParse(pdfs))
 
-embeddings = OpenAIEmbeddings()
+def generatePracticeQuestion(topic, notes, prevQuestions):
+    textSplitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    textSplit = textSplitter.split_text(notes)
+    vectorStore = Chroma.from_texts(textSplit, OpenAIEmbeddings())
 
-vectorStore = Chroma.from_texts(textSplit, embeddings)
+    questionAnswer = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(temperature=0),
+        chain_type="stuff",
+        retriever=vectorStore.as_retriever(search_kwargs={"k": 1})
+    )
 
-questionAnswer = RetrievalQA.from_chain_type(
-    llm=OpenAI(temperature=0),
-    chain_type="stuff",
-    retriever=vectorStore.as_retriever(search_kwargs={"k": 1})
-)
+    query = f"""
+    Only using the context provided, give me 1 practice question
+    that review the content in the context related to the topic, {topic}, with four possible answers 
+    and only one of them is correct. Use the format,
+    Question: (question)
+    A. (answer1)
+    B. (answer2)
+    C. (answer3)
+    D. (answer4)
+    Answer: (answer choice)
 
-query = """
-Only using the context provided, give me 1 practice question
-that review the content in the context with four possible answers 
-and only one of them is correct. Use the format,
-Question: (question)
-A. (answer1)
-B. (answer2)
-C. (answer3)
-D. (answer4)
-Answer: (answer choice)
-"""
+    Don't use any of these questions:
+    {'\n'.join(prevQuestions)}
+    """
+        
+    return questionAnswer.run(query)
 
-result = questionAnswer.run(query)
-print(parse(result))
-print(result)
+print(generatePracticeQuestion(topic, pdfParse(pdfs), []))
--- backend/responseParser.py
+++ backend/responseParser.py
@@ -1,7 +1,11 @@
 def parse(response):
     response = response.split("\n")
+    
+    if response[5] == '':
+        response.pop(5)
+        
     return {
-        "question": response[1][10:],
-        "answers": [response[i][3:] for i in range(2, 6)],
-        "correctAnswer": ord(response[6][8:9]) - ord('A')
+        "question": response[0][10:],
+        "answers": [response[i][3:] for i in range(1, 5)],
+        "correctAnswer": ord(response[5][8:9]) - ord('A')
     }
