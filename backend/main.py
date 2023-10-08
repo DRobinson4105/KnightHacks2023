a6109e3a785e5fbb324a845cf4ae4d90da60f94d
David Robinson <drobinson4105@gmail.com>
2023-10-07 20:40:59 -0400

    Prep backend for merge to test

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,59 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
-from pdfParser import pdfParse
-from responseParser import parse
 from langchain.cache import InMemoryCache
 import langchain
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import PyPDF2
 
-# Setup
+# init
 app = Flask(__name__)
 langchain.llm_cache = InMemoryCache()
 load_dotenv()
 
-@app.route("/api/files", methods=["POST"])
-def generatePracticeQuestion():
+@app.route("/api/getQuestion", methods=["POST"])
+def generateQuestion():
     topic = request.json["topic"]
-    notes = request.files["notes"]
     prevQuestions = request.json["prevQuestions"]
+    
+    # parse PDFs
+    try:
+        notes = ""
+        files = request.files["files"]
+        for file in files:
+            if file.content_type != "application/pdf":
+                return (
+                    jsonify({"error": "Invalid file format. Please upload a PDF file."}),
+                    400,
+                )
+
+            pdf_reader = PyPDF2.PdfReader(file)
+
+            # extract text from each page of pdf
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text() + ' '
+
+            texts += text + ' '
+    except Exception as e:
+        return jsonify({"error": "Error parsing PDF"}), 500
+    
+    # split text into chunks and store in vector db
     textSplitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     textSplit = textSplitter.split_text(notes)
     vectorStore = Chroma.from_texts(textSplit, OpenAIEmbeddings())
 
-    questionAnswer = RetrievalQA.from_chain_type(
+    # setup stuff chain to generate questions
+    generator = RetrievalQA.from_chain_type(
         llm=ChatOpenAI(temperature=0),
         chain_type="stuff",
         retriever=vectorStore.as_retriever(search_kwargs={"k": 1})
     )
 
-    query = f"""
+    prompt = f"""
     Only using the context provided, give me 1 practice question
     that review the content in the context related to the topic, {topic}, with four possible answers 
     and only one of them is correct. Use the format,
@@ -48,4 +70,4 @@ def generatePracticeQuestion():
     for question in prevQuestions:
         query += question + "\n"
         
-    return questionAnswer.run(query)
+    return generator.run(prompt)
